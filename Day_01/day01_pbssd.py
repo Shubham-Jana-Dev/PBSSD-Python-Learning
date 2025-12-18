8dbec64ce54f0b8dddb05b593819fb5b30835724
Shubham Jana <devshubhamjana@gmail.com>
2025-12-18 23:29:51 +0530

    feat: initial commit with Day 1 notes and basic Python syntax script

---
+++ Day_01/day01_pbssd.py
@@ -0,0 +1,19 @@
+name = "Shubham Jana"
+age = 19
+ph_no = "9800000002"
+# type
+print(type(name))
+print(type(age))
+print(type(ph_no))
+# id
+print(id(name))
+print(id(age))
+print(id(ph_no))
+print("------------------------")
+x=2.3
+y=[1,2,3,4,5,6,7]
+print(type(x))
+print(type(y))
+print("------------------------")
+import keyword
+print(keyword.kwlist)
