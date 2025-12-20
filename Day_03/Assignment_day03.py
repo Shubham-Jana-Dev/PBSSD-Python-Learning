e3659305250180aa42187f074f491439c9224f13
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2025-12-20 20:09:06 +0530

    Solve problem number 2

--- Day_03/Assignment_day03.py
+++ Day_03/Assignment_day03.py
@@ -1,6 +1,16 @@
-# find out the area of parallelogram by python Script.
+# 1. find out the area of parallelogram by python Script.
 # formula, Area = base X hight
+'''
 base = float(input("Enter the value of the base : "))
 height = float(input("Enter the value of the height : "))
 area = base*height
 print("The area of the parallelogram is ",area)
+'''
+# 2. Find out the area of conical vessel by python Script.
+'''
+r = float(input("Enter the value of redius : "))
+h = float(input("Enter the value of vertical height : "))
+l = ((r*r)+(h*h))**(1/2)
+area = 3.14*r*(l+r)
+print("The area of the conical vessel is ",area)
+'''
