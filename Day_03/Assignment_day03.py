309ead3a11a47ba899d4ba008a8272eca9886047
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2025-12-20 19:42:30 +0530

    Solving fast question

---
+++ Day_03/Assignment_day03.py
@@ -0,0 +1,6 @@
+# find out the area of parallelogram by python Script.
+# formula, Area = base X hight
+base = float(input("Enter the value of the base : "))
+height = float(input("Enter the value of the height : "))
+area = base*height
+print("The area of the parallelogram is ",area)
