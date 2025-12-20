277a096896269eb1c026f437a38845621f9892b1
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2025-12-20 20:56:13 +0530

    So question number 3

--- Day_03/Assignment_day03.py
+++ Day_03/Assignment_day03.py
@@ -14,3 +14,13 @@ l = ((r*r)+(h*h))**(1/2)
 area = 3.14*r*(l+r)
 print("The area of the conical vessel is ",area)
 '''
+# find out the area of parallelepiped by python script.
+
+a = float(input("Enter the first side (a): "))
+b = float(input("Enter the second side (b): "))
+h = float(input("Enter the height(h) : "))
+face1 = a*b
+face2 = b*h
+face3 = h*a
+area = 2*(face1 + face2 + face3)
+print("The total surface area is ",area)
