7dbd29507e6f84aafb9264ce5c793fa89b5a025c
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2025-12-22 21:13:14 +0530

    Fact: Explore Bina function

--- Day_04/day04_pbssd.py
+++ Day_04/day04_pbssd.py
@@ -1,4 +1,5 @@
 # Python Literals:
+print("-----Python Literals:-----")
 x = {2,5,6}
 y = 45.7
 z = 10 + 20j
@@ -16,7 +17,14 @@ print(type(c))
 print(type(d))
 print(type(e))
 # Binary representation:
+print("-----binary representation:-----")
 k = 0b1011010011
 h = 0B11010011111
 print(k)
 print(h)
+# Exploring bin() Function
+print("-----Explore Bin() function:-----")
+g = 23
+u = 67
+print(bin(g))
+print(bin(u))
