429ea405f4495f0fed215c246f175a6e2dfe90f6
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2025-12-22 21:01:28 +0530

    Exploring Python literals

--- Day_04/day04_pbssd.py
+++ Day_04/day04_pbssd.py
@@ -1,2 +1,17 @@
+# Python Literals:
 x = {2,5,6}
+y = 45.7
+z = 10 + 20j
+a = True
+b = [1,3,4,5]
+c = (1,53,43)
+d = {2,43,54,5}
+e = {5:"A", 6: "Bd"}
 print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
