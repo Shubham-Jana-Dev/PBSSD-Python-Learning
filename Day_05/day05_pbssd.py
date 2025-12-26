b93e3709ba17b1fd3e585751d1cd0a767c6011d8
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2025-12-27 00:52:42 +0530

    Fact: Add day 5 codes

---
+++ Day_05/day05_pbssd.py
@@ -0,0 +1,58 @@
+print ("------01------")
+x = "Shubham"
+y = "Jana"
+z = '''devshubhamjana@gmail.com'''
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+print ("------02------")
+a = '''Shubham
+jana is
+a student'''
+print(a)
+print ("------03------")
+print('''print("^^^^^^^^^^'^^^'"good"")''')
+print(""" '^'6'6'6'6'6'6'6'" '''""")
+print("""''' "_"_"{"{""_""1"'''''''}}''')""")
+print ("------04------")
+name = "Shubhma jana"
+phone_number = "9800000023"
+address = "Kolkata"
+print(f"Name of the person is {name}")
+print(f"phone number of {name} is {phone_number}")
+print(f"address of {name} is {address}")
+print ("------05------")
+x,y,z = input("Enter the values : - ").split("8")
+print(x)
+print(y)
+print(z)
+print ("------06------")
+x = 0x12b
+print(x,type(x))
+x = 74844
+y = 354271
+z = 90742750
+k = hex(x)
+l = hex(y)
+m = hex(z)
+print(k,type(k))
+print(l,type(l))
+print(m,type(m))
+a = oct(x)
+b = oct(y)
+c = oct(z)
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+print ("------07------")
+x = float()
+print(x)
+print ("------08------")
+v = 34.55
+print(v,type(v))
+e = int(x)
+print(e,type(e))
+print ("------09------")
+x1 = float(ord('a'))
+print(x1)
+print(type(x1))
