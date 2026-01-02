b43758023e571638831aadf8453af00bdc57a087
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-02 22:12:11 +0530

    modified:   Day_08/day08_pbssd.py #

--- Day_08/day08_pbssd.py
+++ Day_08/day08_pbssd.py
@@ -3,3 +3,14 @@ x = 16.4
 print(x,type(x))
 y = int(x)
 print(y,type(y))
+# Explicit type casting of the complex numbers
+c = 13 + 43j
+print(c)
+print(type(c))
+z = x.real
+print(z,type(z))
+a = x.imag
+print(a,type(a))
+
+# but one thisng 
+# c = int(x)
