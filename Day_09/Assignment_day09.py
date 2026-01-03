ed7b5e4114c490fef0ab2fe95a8f79c1cf9eda6a
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-03 20:52:31 +0530

    feat: add progressive income tax calculator with slab-based logic

--- Day_09/Assignment_day09.py
+++ Day_09/Assignment_day09.py
@@ -15,3 +15,27 @@ elif(opereter == '/'):
         print(f"any number cannot be divisible by zero.")
 else:
     print("You have chosen a invalid input as the oparetor. :(")
+#Calculate income tax based on salary slabs using if-elif.
+print("""
+income >= 10000 = 0%
+10000 < income <= 50000 = 15%
+50000 < income <= 80000 = 20%
+80000 < income <= 100000 = 25%
+100000 < income = 30%
+      """)
+income = float(input("Enter your tax : "))
+tax = 0
+if (income <= 10000):
+    print("No tax :)")
+elif ( income <= 50000):
+    tax = (income - 10000)* 0.15
+    print(f"HI Unfortunately you have to pay ₹ {tax} as tax :)")
+elif (income <= 80000):
+    tax = (40000 * 0.15) + (income - 50000) * 0.2
+    print(f"HI Unfortunately you have to pay ₹ {tax} as tax :)")
+elif (income <= 100000):
+    tax = (40000 * 0.15) + (30000 * 0.2) + (income - 80000)*0.25
+    print(f"HI Unfortunately you have to pay ₹ {tax} as tax :)")
+else:
+    tax = (40000 * 0.15) + (30000 * 0.2) + (20000*0.25) + (income - 100000)*0.3
+    print(f"HI Unfortunately you have to pay ₹ {tax} as tax :)")
