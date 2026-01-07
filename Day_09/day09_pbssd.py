630245e1a0875fd08f94f5f43d648f69197ac22a
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-07 20:47:18 +0530

    feat: build multi-functional calculator with zero-division protection

--- Day_09/day09_pbssd.py
+++ Day_09/day09_pbssd.py
@@ -28,3 +28,42 @@ if char >= 'A' and char <= 'Z':
 else:
     print("Lower case")
 '''
+#
+'''
+char = input("Enter it ....")
+if('0'<=char<='9'):
+    print("It's a number")
+else:
+    print("It's a charecter")
+'''
+#
+'''
+x = input("Enter it...")
+if(x>='A' and 'Z'>=x):
+    print("it's a upper case charecter")
+elif('a'<=x<='z'):
+    print("It's a lower case charecter")
+elif(x>='0' and x<='9'):
+    print("It's a digit")
+else:
+    print("It's a special charecter")
+'''
+# build multi-functional calculator with zero-division protection
+x = int(input("Enter the number: "))
+y = int(input("Enter the number: "))
+opareter = input("chose the opareter (+,-,*,/,%):-")
+if(opareter == '+'):
+    print(f"{x} + {y} = ",x+y)
+elif(opareter == '-'):
+    print(f"{x} - {y} = ",x-y)
+elif(opareter == '*'):
+    print(f"{x} * {y} = ",x*y)
+elif(opareter == '/'):
+    if(y!=0):
+        print(f"{x} / {y} = ",x/y)
+    else:
+        print("please enter a valid input")
+elif(opareter == '%'):
+    print(f"{x} % {y} = ",x%y)
+else:
+    print("please chose between +,-,*,/,%")
