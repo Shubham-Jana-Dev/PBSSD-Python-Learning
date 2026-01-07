7a29c37ecde40abf4139637cba639ef3ef828439
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-07 20:35:59 +0530

    feat: add nested if logic for positive/negative even/odd analysis

--- Day_09/day09_pbssd.py
+++ Day_09/day09_pbssd.py
@@ -1,5 +1,20 @@
+# Check a digit vs character recognition
+'''
 e = input("Enter a char....")
 if('0'<=e<='9'):
     print("It's a digit")
 else:
     print("It's a charecter")
+'''
+# positive/negative even/odd analysis
+x = int(input("Enter number:-"))
+if(x>=0):
+    if(x%2 == 0):
+        print("Positive and even")
+    else:
+        print("Positive and odd")
+else:
+    if(x%2 == 0):
+        print("Nagetive and even")
+    else:
+        print("Nagetive and odd")
