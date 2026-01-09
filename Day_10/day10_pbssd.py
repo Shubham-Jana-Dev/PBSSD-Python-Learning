af4e6ab256c91be7755e5329cf95026550893581
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-09 22:01:42 +0530

    docs: define iterative statements and while loop syntax

---
+++ Day_10/day10_pbssd.py
@@ -0,0 +1,17 @@
+x = int(input("Enter start value (x): "))
+y = int(input("Enter end value (y): "))
+
+no_even = 0; no_odd = 0; sum_even = 0; sum_odd = 0
+
+while x <= y:  
+    if x % 2 == 0:
+        print("Even number = ", x)
+        no_even += 1
+        sum_even += x
+    else:
+        print("Odd number = ", x)
+        no_odd += 1
+        sum_odd += x
+    x += 1  
+print(f"Total Even: {no_even}, Sum: {sum_even}")
+print(f"Total Odd: {no_odd}, Sum: {sum_odd}")
