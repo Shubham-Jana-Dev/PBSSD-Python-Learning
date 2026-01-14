894acb6619a0c71b19c2227e21d6a9aee702ac2d
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-14 21:36:42 +0530

    feat: implement logic to identify Armstrong numbers

---
+++ Day_12/day12_pbssd.py
@@ -0,0 +1,17 @@
+# --- Armstrong Number Logic ---
+num = int(input("Enter the number: "))
+temp1 = num
+temp2 = num 
+length = len(str(num)) # Efficient way to get length
+total_sum = 0
+
+# Calculating Armstrong Sum
+while temp2 != 0:
+    rem = temp2 % 10
+    total_sum += (rem ** length)
+    temp2 //= 10
+
+if total_sum == num: # Fixed: use == for comparison
+    print(f"{num} is an Armstrong number")
+else:
+    print(f"{num} is NOT an Armstrong number")
