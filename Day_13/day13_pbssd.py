78f25d3e2764731b355ac382ce064b187b168385
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-15 22:08:48 +0530

    feat: reverse all the numbers between a perticulal range.

--- Day_13/day13_pbssd.py
+++ Day_13/day13_pbssd.py
@@ -1,4 +1,5 @@
 # 1. Print the factorial of all numbers between a range.
+"""
 x = int(input("Enter the first range: "))
 y = int(input("Enter the last range: "))
 if x<y:
@@ -11,3 +12,15 @@ if x<y:
         print(f" The factorial of {i} = {fact}.")
 else:
     print("Please enter the valid order.")
+"""
+# 2. Reverse all the numbers between a Particular range
+num1 = int(input("Enter the first range: "))
+num2 = int(input("Enter the last range: "))
+for i in range(num1,num2 + 1):
+    temp = i
+    rev = 0
+    while temp != 0:
+        rem = temp%10
+        rev = (rev * 10) + rem
+        temp //= 10
+    print(f"The reverse number of {i} = ",rev)
