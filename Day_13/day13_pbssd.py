f0a06345ea7e108905da384ebafc153e1ce87f27
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-17 19:15:38 +0530

    feat: Understand and  implement the nested loops by using Matrix

--- Day_13/day13_pbssd.py
+++ Day_13/day13_pbssd.py
@@ -42,6 +42,7 @@ if x<y:
                 print(i) 
 """
 # Check a number is amstrong numbner or not.
+'''
 num = int(input("Enter the number: "))
 temp = num 
 temp1 = num
@@ -59,7 +60,9 @@ if(total_sum == num):
     print(f"{num} is an amstrong number.")
 else:
     print(f"{num} is not an amstrong number.")
+'''
 # class work 4 :- Write a program to print all amstrong numbers between a range
+'''
 num1 = int(input("Enter the starting range: "))
 num2 = int(input("Enter the ending range: "))
 temp = 1
@@ -79,5 +82,44 @@ if(num1<num2):
             temp2 //= 10
         if(total_sum == i):
             print(i)
+'''
+# Nested Loops
+"""
+In this setup, for every single iteration of the "outer" loop, the "inner" loop runs through its entire cycle from start to finish.
 
-    
+How it Works:
+Think of it like the hands on a mechanical clock:
+The Outer Loop is the hour hand (moves slowly).
+The Inner Loop is the minute hand (completes a full circle for every single step the hour hand takes).
+"""
+# Basic nested loop understanding
+'''
+for i in range(1,3):
+    for j in range(1,3):
+        print(i," ",j)
+'''
+# understanding the matrix by the nested loop
+"""
+for i in range(1,3):
+    for j in range(1,3):
+        for k in range(1,3):
+            print("i=",i," j=",j," k=",k)
+"""
+# Understanding a little bit complex nested loop
+'''
+r = int(input("Enter the range: "))
+print("The entered size of the matrix: ",r)
+for i in range(1,r+1):
+    for j in range(1,r+1):
+        print("i=",i)
+    print("*j=",j)
+'''
+#
+"""
+Range = int(input("Enter the Range: "))
+print("The entered size of matrix is: ",Range)
+for i in range(1,Range+1):
+    for j in range(1,Range+1):
+        print("i=",i,"j=",j," ",end=" ")
+    print(" ")
+"""
