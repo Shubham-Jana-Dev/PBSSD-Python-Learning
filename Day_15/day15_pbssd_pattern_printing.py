aa360fc3221bac7f72a74ebf9e75ca7436d16bb0
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-19 21:57:16 +0530

    feat: write the program to print the partten of the 'Z'

--- Day_15/day15_pbssd_pattern_printing.py
+++ Day_15/day15_pbssd_pattern_printing.py
@@ -32,11 +32,20 @@ for i in range(1,Rg+1):
 
 *                               *
 '''
-
+'''
 Rg = int(input("Enter the range: "))
 for i in range(1,Rg+1):
     for j in range(1,Rg+1):
         if j == Rg or j == 1 or j+i==Rg+1:
+            print("*",end="\t")
+        else:
+            print(" ",end="\t")
+    print("\n")
+'''
+Rg = int(input("Enter the range: "))
+for i in range(1,Rg+1):
+    for j in range(1,Rg+1):
+        if i == Rg or i == 1 or j+i==Rg+1 :
             print("*",end="\t")
         else:
             print(" ",end="\t")
