a4d8688b4ee776752fbc0d1b25cab9407b6e3515
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-19 22:18:04 +0530

    feat: implement X-pattern logic using diagonal and anti-diagonal conditions

--- Day_15/day15_pbssd_pattern_printing.py
+++ Day_15/day15_pbssd_pattern_printing.py
@@ -42,10 +42,33 @@ for i in range(1,Rg+1):
             print(" ",end="\t")
     print("\n")
 '''
+#################################################################################################################
+"""
+*       *       *       *       *
+
+                        *        
+
+                *                
+
+        *                        
+
+*       *       *       *       *
+"""
+"""
 Rg = int(input("Enter the range: "))
 for i in range(1,Rg+1):
     for j in range(1,Rg+1):
         if i == Rg or i == 1 or j+i==Rg+1 :
+            print("*",end="\t")
+        else:
+            print(" ",end="\t")
+    print("\n")
+"""
+#################################################################################################################
+h = int(input("Enter the range: "))
+for i in range(1,h+1):
+    for j in range(1,h+1):
+        if j == 1 or j == h or i+j == h+1 or j==i:
             print("*",end="\t")
         else:
             print(" ",end="\t")
