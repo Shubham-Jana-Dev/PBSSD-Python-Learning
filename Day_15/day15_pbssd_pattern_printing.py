ea750cfd617a879c9a463a0fc7b6593a98260dc6
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-19 21:54:52 +0530

    feat: Add the program to print the pattern of reversed 'N'

--- Day_15/day15_pbssd_pattern_printing.py
+++ Day_15/day15_pbssd_pattern_printing.py
@@ -21,3 +21,23 @@ for i in range(1,Rg+1):
     print("\n")
 """
 #################################################################################################################
+'''
+*                               *
+
+*                       *       *
+
+*               *               *
+
+*       *                       *
+
+*                               *
+'''
+
+Rg = int(input("Enter the range: "))
+for i in range(1,Rg+1):
+    for j in range(1,Rg+1):
+        if j == Rg or j == 1 or j+i==Rg+1:
+            print("*",end="\t")
+        else:
+            print(" ",end="\t")
+    print("\n")
