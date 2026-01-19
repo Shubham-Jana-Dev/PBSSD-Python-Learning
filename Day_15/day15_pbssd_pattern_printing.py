cf86215d5e4f7de6b59ec6880101d76d3eb76a2f
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-20 04:50:56 +0530

    style: refine output formatting by replacing tabs with consistent spacing

--- Day_15/day15_pbssd_pattern_printing.py
+++ Day_15/day15_pbssd_pattern_printing.py
@@ -1,75 +1,89 @@
 """
-*                               *
-
-*       *                       *
-
-*               *               *
-
-*                       *       *
-
-*                               *
-
+*       *  
+* *     *  
+*   *   *  
+*     * *  
+*       *  
 """
 """
 Rg = int(input("Enter the range: "))
 for i in range(1,Rg+1):
     for j in range(1,Rg+1):
         if j == Rg or j == 1 or j==i:
-            print("*",end="\t")
+            print("*",end=" ")
         else:
-            print(" ",end="\t")
-    print("\n")
+            print(" ",end=" ")
+    print(" ")
 """
 #################################################################################################################
 '''
-*                               *
-
-*                       *       *
-
-*               *               *
-
-*       *                       *
-
-*                               *
+*       *  
+*     * *  
+*   *   *  
+* *     *  
+*       * 
 '''
 '''
 Rg = int(input("Enter the range: "))
 for i in range(1,Rg+1):
     for j in range(1,Rg+1):
         if j == Rg or j == 1 or j+i==Rg+1:
-            print("*",end="\t")
+            print("*",end=" ")
         else:
-            print(" ",end="\t")
-    print("\n")
+            print(" ",end=" ")
+    print(" ")
 '''
 #################################################################################################################
 """
-*       *       *       *       *
-
-                        *        
-
-                *                
-
-        *                        
-
-*       *       *       *       *
+* * * * *  
+      *    
+    *      
+  *        
+* * * * * 
 """
 """
 Rg = int(input("Enter the range: "))
 for i in range(1,Rg+1):
     for j in range(1,Rg+1):
         if i == Rg or i == 1 or j+i==Rg+1 :
-            print("*",end="\t")
+            print("*",end=" ")
         else:
-            print(" ",end="\t")
-    print("\n")
+            print(" ",end=" ")
+    print(" ")
 """
 #################################################################################################################
+'''
+*       *  
+* *   * *  
+*   *   *  
+* *   * *  
+*       * 
+'''
+'''
 h = int(input("Enter the range: "))
 for i in range(1,h+1):
     for j in range(1,h+1):
         if j == 1 or j == h or i+j == h+1 or j==i:
-            print("*",end="\t")
+            print("*",end=" ")
         else:
-            print(" ",end="\t")
-    print("\n")
+            print(" ",end=" ") 
+    print(" ")
+'''
+#################################################################################################################
+"""
+* * * * *  
+  *   *    
+    *      
+  *   *    
+* * * * *
+"""
+"""
+t = int(input("Enter the range: "))
+for i in range(1,t+1):
+    for j in range(1,t+1):
+        if j == i or i == 1 or i+j == t+1 or i == t:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print(" ")
+"""
