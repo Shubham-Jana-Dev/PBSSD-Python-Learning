a8ce016bba8cb31888e8935b75c2e1549ddb1c53
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-20 05:47:03 +0530

    feat: implement complex internal diagonal logic and bounded star patterns

--- Day_15/day15_pbssd_pattern_printing.py
+++ Day_15/day15_pbssd_pattern_printing.py
@@ -160,3 +160,37 @@ for i in range(1,t+1):
     print(" ")
 """
 #################################################################################################################
+'''
+* * * * *  
+*   *   *  
+*   *   *  
+* *   * *  
+* * * * * 
+'''
+'''
+f = int(input("Enter the range: "))
+for i in range(1, f+1):
+    for j in range(1,f+1):
+        if i == 1 or j == 1 or i == f or j == f or (i+j == f+1 and i>=j) or (i == j and i+j>=f+1) or (j == (f+1)/2 and j>i):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print(" ")
+'''
+"""
+* * * * *  
+* *   * *  
+*   *   *  
+*   *   *  
+* * * * *  
+"""
+"""
+f = int(input("Enter the range: "))
+for i in range(1,f+1):
+    for j in range(1,f+1):
+        if i == 1 or j == 1 or i == f or j == f or i == j and j+i <= f+1 or i+j == f+1 and i<j or j == (f+1)/2 and j < i :
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print(" ")
+"""
