43a33a73629c9834496d93a99add867b9969cb75
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-26 08:04:21 +0530

    feat: print name using function

---
+++ Day_16/day16_pbssd_functions.py
@@ -0,0 +1,17 @@
+# finctions 
+# printing my name by using function call
+'''
+def printing_name():
+    print("Shubham Jana")
+printing_name()
+printing_name()
+printing_name()
+printing_name()
+'''
+# another way
+"""
+def name():
+    print("Shubham Jana")
+for i in range(100):
+    name()
+"""
