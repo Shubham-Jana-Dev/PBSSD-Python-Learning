b847ee47ff5242eeca577dd2fa0d95f5b7ef8f66
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-26 08:32:44 +0530

    feat: create a besic calculater using function

--- Day_16/day16_pbssd_functions.py
+++ Day_16/day16_pbssd_functions.py
@@ -15,3 +15,23 @@ def name():
 for i in range(100):
     name()
 """
+def calculator():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    operator = input("Enter the operator ('+','-','/','%') : ")
+    if (operator == '+'):
+        print(f"{num1} + {num2} = ",num1+num2)
+    elif(operator == '-'):
+        print(f"{num1} - {num2} = ", num1-num2)
+    elif(operator == '*'):
+        print(f"{num1} X {num2} = ", num1*num2)
+    elif(operator == '/'):
+        if(num2 != 0):
+            print(f"{num1} ÷ {num2} = ", num1/num2)
+        else:
+            print("Please enter a valid a input because no numbeer can not divisible by 0. :(")
+    elif(operator == '%'):
+        print(f"the remainder of {num1} ÷ {num2} is ", num1%num2)
+    else:
+        print("Please enter a valid operator.")
+calculator()
