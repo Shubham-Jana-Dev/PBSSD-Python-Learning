a9b52d0493dd105d603f0525d23ec2717861eb62
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-26 12:38:34 +0530

    feat: understand lambda function or anonymous function

--- Day_16/day16_pbssd_functions.py
+++ Day_16/day16_pbssd_functions.py
@@ -1,4 +1,8 @@
 # finctions 
+# There is mainly 2 types of functions
+# 1. Built in function (print(),rnage(),sum(),len(),etc)  
+# 2. User defined function (crated by users for some spcific requirments)
+# rules of naming a function is similer to the rules of naming a variable
 # printing my name by using function call
 '''
 def printing_name():
@@ -77,6 +81,7 @@ happy_birth_day("Shubham",19)
 happy_birth_day("Rahul",21)
 '''
 # returning string
+"""
 def My_name(first_name,last_name):
     first_name =first_name.capitalize()
     last_name = last_name.capitalize()
@@ -91,3 +96,44 @@ def calculate_school_fees(amount,time):   # the pass keyword do nothing inside a
     pass
 School_name = My_name("La","Martinieres for boys")
 print(School_name)
+"""
+# Anonymous function (or Lambda functions)
+# This function has not names... that's why we call is anonymous function 
+# A lambda function can have multiple parameters....
+# A lambda function must be written in a single line of code....
+# The syntax of lambda function is diffrent from a ragular functions....
+# A lambda function does not need a return statment....
+'''
+syntax : 
+variable_name = lanmbda parametes: expression
+'''
+dubb = lambda x: x*2
+print(dubb(3)) # using a single paramerter
+
+addition = lambda x,y: x+y
+print(addition(4,3)) # using multiple parameters
+
+max_value = lambda t,z: t if t>z else z # we can also write conditional statements using lambda functions
+print(max_value(5,8))
+
+full_name = lambda first,last: first + " " + last # we can use lambda function to concatinate 2 strings
+print(full_name("Shubham","Jana"))
+
+is_even = lambda x : "Even" if x%2 == 0 else "Odd"
+print(is_even(8))
+# or we can write
+is_odd = lambda x: x%2 !=0
+print(is_odd(6))
+"""
+def square (num) :
+    return num ** 2
+def cube (num) :
+    return num ** 3
+def transform_list(num_list,transform_items):
+    transformed_01 = transform_items(num_list[0])
+    transformed_02 = transform_items(num_list[1])
+    return[transformed_01,transformed_02]
+my_list = [2, 3]
+e = transform_list (my_list, square)
+print(e)
+"""
