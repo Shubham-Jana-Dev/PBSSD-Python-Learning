c8d2abf353d54d6b112fe8a6c0fe371765dda1e0
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-02-01 13:14:41 +0530

    feat: implement map and filter with lambda for list transformations

--- Day_17/day17_pbssd_function.py
+++ Day_17/day17_pbssd_function.py
@@ -6,4 +6,18 @@ def understand_higher_order_fun():
     even_only = list(filter(lambda num : num%2 == 0,p))   # it would help to fillter out the odd numbers
     print(even_only)
     # return p , even_only
-understand_higher_order_fun()
+# understand_higher_order_fun()
+def squre_number():
+    num = int(input("Enter the number: "))
+    x = lambda n : n*n
+    print(f"The squre of the {num} is {x(num)}")
+# squre_number()
+def apply_operation(numbers, func):
+    return[func(n) for n in numbers]
+my_nums = [1,2,3,4,5,6]
+squres = apply_operation(my_nums,lambda n : n**2)
+cubes = apply_operation(my_nums, lambda n : n**3)
+print("Squres : ",squres)
+print("Cubes : ",cubes)
+
+
