c771d1169ccb4a5c67f5afe75d1ccdfce6c13e21
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-02-04 19:05:04 +0530

    feat: modification of sets

--- Day_19/say19_python_myself_List_Set_Disc.py
+++ Day_19/say19_python_myself_List_Set_Disc.py
@@ -31,17 +31,58 @@ The main difference between lists and tupples is, lists are mutable means we can
 or we can add elements (.append) or remove elements (.remove).
 but we cant any operation in the tupple so sets are immutable.
 '''
-def exploring_add_in_list(the_lis,new_e):
+def exploring_append_in_list(the_lis,new_e):
     the_lis.append(new_e)
     return the_lis
 the_list = [1,2,3,4,5,6]
 New_element = 7
-result01 = exploring_add_in_list(the_list,New_element)
+result01 = exploring_append_in_list(the_list,New_element)
 # print(result01)
-def exploring_add_in_list(the_lis,new_e):
+
+
+def exploring_remove_in_list(the_lis,new_e):
     the_lis.remove(new_e)
     return the_lis
 the_list02 = [10,62,43,5,54,5,60]
 Old_element = 5
-result02 = exploring_add_in_list(the_list02,Old_element)
-print(result02)
+result02 = exploring_remove_in_list(the_list02,Old_element)
+# print(result02)
+
+
+def exploring_add_in_set(the_set01,new_element01):
+    the_set01.add(new_element01)
+    return the_set01
+the_list02 = {1,2,3,4,5,6,7}
+New_element = 8
+result022 = exploring_add_in_set(the_list02,New_element)
+print(result022)
+
+
+def exploring_remove_in_set(the_set02,old_element02):
+    the_set02.remove(old_element02)
+    return the_set02
+the_list02 = {1,2,3,4,5,6,7}
+Old_element = 6
+result022 = exploring_remove_in_set(the_list02,Old_element)
+print(result022)
+
+
+def update_index_from_list(y_list,index_num,new_element99):
+    y_list[index_num] = new_element99
+    return y_list
+lis = [44,3,21,1,2,3,434,2,6,4]
+update_requireAt = 6
+new_number_should_be = 55
+result99 = update_index_from_list(lis,update_requireAt,new_number_should_be)
+print(result99)
+
+
+def update_index_from_set(y_set,index_num,new_element991):
+    y_set.remove(index_num)
+    y_set.add(new_element991)
+    return y_set
+set1 = {44,3,21,1,22,443,434,2,6,4}
+unwanted = 3
+new_number_should_be = 55
+result991 = update_index_from_set(set1,unwanted,new_number_should_be)
+print(result991)
