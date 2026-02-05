0979d6d5fb5e960eaf98a69513d8b51f04bb596a
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-02-05 06:17:08 +0530

    feat: ValueError Handling and use recursion for first time in this repo :)

--- Day_20/day20_python_myself_ErorrHandling.py
+++ Day_20/day20_python_myself_ErorrHandling.py
@@ -5,4 +5,16 @@ def NameErrorHandling():
         print(a + b)
     except NameError:
         print("Invalid input. :( \n ! Please enter a valid input for a and b. :) ")
-NameErrorHandling()
+# NameErrorHandling()
+
+
+def ValueErrorHandling():
+    try:
+        a = int(input("Enter the number: "))
+        b = int(input("Enter the number: "))
+        print(f"{a} + {b} = ",a+b)
+    except ValueError:
+        print("Please Enter integer value. :(")
+        ValueErrorHandling()
+# ValueErrorHandling()
+
