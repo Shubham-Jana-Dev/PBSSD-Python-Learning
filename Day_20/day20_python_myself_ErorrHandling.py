25bb4c6899c81205efa77b07acf2b7cafd5c21d8
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-02-05 06:36:33 +0530

    feat: ValueError handling

--- Day_20/day20_python_myself_ErorrHandling.py
+++ Day_20/day20_python_myself_ErorrHandling.py
@@ -1,6 +1,6 @@
 def NameErrorHandling():
     try:
-        a = o
+        a = o # I have use string (without " ") for creating the NameError.
         b = 23
         print(a + b)
     except NameError:
@@ -10,11 +10,13 @@ def NameErrorHandling():
 
 def ValueErrorHandling():
     try:
-        a = int(input("Enter the number: "))
+        a = int(input("Enter the number: "))  # use a float value for better understing (for creating the ValueError)
         b = int(input("Enter the number: "))
         print(f"{a} + {b} = ",a+b)
     except ValueError:
         print("Please Enter integer value. :(")
         ValueErrorHandling()
 # ValueErrorHandling()
-
+a = int(input("Enter the number: "))  # use a float value for better understing (for creating the ValueError)
+b = int(input("Enter the number: "))
+print(f"{a} + {b} = ",a+b)
