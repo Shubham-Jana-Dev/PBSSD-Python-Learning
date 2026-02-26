323775667708aac6bd98f5e02076ade9ffcab4f6
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-02-26 21:15:01 +0530

    feat: access the first and the last element of a list

--- Day_26/day26_inbuilt_functions_of_list.py
+++ Day_26/day26_inbuilt_functions_of_list.py
@@ -6,4 +6,23 @@ def len_calculation_using_loop():
     for i in My_list:
         count += 1
     print(count)
-len_calculation_using_loop()
+# len_calculation_using_loop()
+
+# Using inbuilt function
+def using_inbuilt_func():
+    My_list = [22,43,5,76,33,'Shubham',2036]
+    print(len(My_list))
+# using_inbuilt_func()
+
+# 2. How could you access first and the last element of a list.
+def access_first_and_last():
+    My_list = ['public Bus',1,1.1,1.2,'.....',99.8,99.9,100,'BMW']
+    print(f"The first element = {My_list[0]}\nThe last element = {My_list[len(My_list)-1]}")
+# access_first_and_last()
+
+# 3. Write a program to add a element at the end of a list.
+def element_add_at_last():
+    My_list = ['laptop','the potential','phone','classes']
+    My_list.append('The Why')
+    print(My_list)
+# element_add_at_last()
