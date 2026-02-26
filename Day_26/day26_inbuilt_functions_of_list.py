301897582f400544aaadf5a5155e92b4b6961233
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-02-27 04:11:31 +0530

    feat: add exampls of the functions insert,remove,max,count

--- Day_26/day26_inbuilt_functions_of_list.py
+++ Day_26/day26_inbuilt_functions_of_list.py
@@ -26,3 +26,38 @@ def element_add_at_last():
     My_list.append('The Why')
     print(My_list)
 # element_add_at_last()
+
+# 4. Write a element to insert a new element in a specific position inside a list.
+def element_insert():
+    My_list = ['fingers','vision','vs code','70 wpm']
+    My_list.insert(2,'hunger of exploration')
+    print(My_list)
+# element_insert()
+
+# 5. Write a program to remove an element from a list.
+def remove_form_list():
+    My_list = ['PBSSD','python','late night coding','sleep']
+    My_list.remove('sleep')
+    print(My_list)
+# remove_form_list()
+
+# 6. Write a program to find out the maxium element.
+# Using the loop
+def maximum_element():
+    t=0
+    marks = [89,90,96,99,87,100,95]
+    for i in marks:
+        if t<i:
+            t = i
+    print(t)
+# maximum_element()
+def max_func():
+    marks = [89,90,96,99,87,100,95]
+    print(max(marks))
+# max_func()
+
+# 7. Write a program to count how many times an element appears in the list?
+def using_count_func():
+    my_list = [1,2,3,2,4,5,2,6,5,2,10]
+    print(my_list.count(2)) 
+using_count_func()       
