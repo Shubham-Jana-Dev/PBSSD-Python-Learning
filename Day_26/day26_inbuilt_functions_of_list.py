f47471f3ba2226d1b0044fb52e9e96a2b4a373aa
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-02-26 20:32:45 +0530

    feat: calculate the length of a list

---
+++ Day_26/day26_inbuilt_functions_of_list.py
@@ -0,0 +1,9 @@
+# 1. write a program to find the length of a list
+# Using loop
+def len_calculation_using_loop():
+    My_list = [12,13,14,17,51,33]
+    count = 0
+    for i in My_list:
+        count += 1
+    print(count)
+len_calculation_using_loop()
