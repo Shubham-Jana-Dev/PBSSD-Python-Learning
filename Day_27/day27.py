52776b35123c11e315838efd1b2a0585b8545c32
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-03-01 06:06:40 +0530

    feat: add program for finding the smallest and the largest value from a list and filter elements by list comprehention from a given list

--- Day_27/day27.py
+++ Day_27/day27.py
@@ -7,7 +7,26 @@ def using_loop():
             blank.append(i)
     print(blank)
 # using_loop()
+
 def by_list_comprehension():
     the_list = [i for i in My_list if i%5 !=0]
     print(the_list)
-by_list_comprehension()
+# by_list_comprehension()
+
+# Find out the maximum element from a list.
+def maximum_element():
+    print(max(My_list))
+# maximum_element()
+
+# Find out the minimum element from a list.
+def minimum_element():
+    print(min(My_list))
+# minimum_element()
+
+# names = ['Shubham','Surajit','Arjun','Sneha','Karan','Sinchan','Doremon','Sisimanu'] filter out the name which starts with 'S' from the given list.
+def filter_s_names():
+    names = ['Shubham','Surajit','Arjun','Sneha','Karan','Sinchan','Doremon','Sisimanu']
+    s_names = [n for n in names if 'S' in n]
+    print(s_names)
+filter_s_names()
+
