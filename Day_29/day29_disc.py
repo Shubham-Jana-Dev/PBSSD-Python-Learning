44bcad90f778a0bf5125e7c2cc3a58dc285adb8d
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-03-02 05:06:24 +0530

    feat: print all keys() of a dictionary and add a conditional statement to check if a key is present in the dictionary or npt by using the membership operator

--- Day_29/day29_disc.py
+++ Day_29/day29_disc.py
@@ -9,3 +9,16 @@ def the_challange():
     print(attendance_log["February"])
 # the_challange()
 
+def the_future():
+    t_f = {"Year":2036,"Age":29,"Role":"senior developer"}
+    print(t_f.keys())
+# the_future()
+
+def find_success():
+    f_s = {"Year":2036,"Age":29,"Role":"senior developer"}
+    f_s["Status"] = "Success"
+    if "Status" in f_s:
+        print("I have achieved it.")
+    else:
+        print("Still under process.")
+find_success()
