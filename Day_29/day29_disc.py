db69ff18bc8f507b8173b2a85006fbedca7fb6d6
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-03-02 04:44:43 +0530

    feat: Add the day 29 and practice some basic operation in dictionary.

---
+++ Day_29/day29_disc.py
@@ -0,0 +1,11 @@
+def disc_s():
+    My_info = {'name':'Shubham','age':19,'role':'Student','current year':2025}
+    My_info['current year'] = 2026
+    print(My_info)
+
+def the_challange():
+    attendance_log = {"Jannuary":100,"February":100,"March":100}
+    attendance_log["April"] = 100
+    print(attendance_log["February"])
+# the_challange()
+
