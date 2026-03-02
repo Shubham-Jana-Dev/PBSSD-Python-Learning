6e4eb2d668610293ed64e1a537130dcf8b2d4044
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-03-02 05:50:26 +0530

    feat: practice nested  Dictionary and check values inside the child dictionary

--- Day_29/day29_disc.py
+++ Day_29/day29_disc.py
@@ -2,6 +2,7 @@ def disc_s():
     My_info = {'name':'Shubham','age':19,'role':'Student','current year':2025}
     My_info['current year'] = 2026
     print(My_info)
+# disc_s()
 
 def the_challange():
     attendance_log = {"Jannuary":100,"February":100,"March":100}
@@ -21,4 +22,14 @@ def find_success():
         print("I have achieved it.")
     else:
         print("Still under process.")
-find_success()
+# find_success()
+
+def student_status():
+    student_info = {"Ram":{'status':'Pass'},"Bikash":{'status':'fail'},"Neha":{'status':'Pass'},"Rahul":{'status':'fail'},"Sukamal":{'status':'Pass'}}
+    count = 0
+    for i in student_info:
+        if student_info[i].get("status") == "Pass":  #.get('status')
+            print(i)
+            count += 1
+    print(f"The total number of passed students: {count}")
+# student_status()
