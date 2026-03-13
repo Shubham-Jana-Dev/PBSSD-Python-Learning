5e184d5c8c289211806fa0d92f82e178ce2dcfd6
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-03-13 06:52:26 +0530

    feat:solved the problem in O(n) time complexity

--- Day_32/day_32.py
+++ Day_32/day_32.py
@@ -6,15 +6,35 @@ def basic_recap():
     print(dictA["car2"])
     print(dictA)
 
-'''
-The Challenge: The "Audit Log" Optimizer
-​You are building a high-frequency trading system. You receive a list of transactions (logs) as a list of dictionaries. Each dictionary contains: {'user_id': int, 'timestamp': int, 'action': str, 'metadata': dict}.
-​Your Task:
-Write a function that processes 100,000 logs and returns a Nested Dictionary Summary that maps:
-​Each user_id to their most frequent action.
-​Each user_id to the total number of unique keys present in all of their metadata dictionaries combined.
-​⚔️ The Constraints (Technical Brutality):
-​Time Complexity: You must achieve O(N + U \cdot M), where N is the number of logs, U is the number of unique users, and M is the average number of unique metadata keys. If you use a nested loop that results in O(N^2), you’ve failed.
-​Memory Efficiency: Do not create redundant copies of the log list. Use collections.defaultdict or collections.Counter to keep the logic lean.
-​Architect Optimization: Handle the case where a user has a "tie" for the most frequent action. How do you decide which one to return? (Hint: Use the most recent one based on timestamp).
-'''
+from collections import defaultdict, Counter
+
+def optimize_logs(logs):
+    user_actions = defaultdict(Counter)
+    user_metadata_keys = defaultdict(set)
+    user_leaders = {}
+    for log in logs:
+        uid = log['user_id']
+        act = log['action']
+        ts = log['timestamp']
+        meta = log['metadata']
+        user_actions[uid][act] += 1
+        user_metadata_keys[uid].update(meta.keys())
+        current_count = user_actions[uid][act]
+        if uid not in  user_leaders:
+            user_leaders[uid] = (current_count, ts, act)
+        else:
+            leader_count, leader_ts, leader_act = user_leaders[uid]
+            if current_count > leader_count or (current_count == leader_count and ts > leader_ts):
+                user_leaders[uid] = (current_count, ts, act)
+    return{
+            uid: {
+                "top_action": user_leaders[uid][2],
+                "unique_metadata_count":len(user_metadata_keys[uid])
+            } for uid in user_actions
+        }
+logs = [
+    {'user_id': 1, 'action': 'login', 'timestamp': 100, 'metadata': {'ip': '1.1.1.1'}},
+    {'user_id': 1, 'action': 'click', 'timestamp': 105, 'metadata': {'btn': 'buy'}},
+    {'user_id': 1, 'action': 'login', 'timestamp': 110, 'metadata': {'ip': '1.1.1.1', 'device': 'mobile'}}
+]
+print(optimize_logs(logs))
